7b888a7a977c5ece02f372fe1dddd0faaeb83a8f
Ahmad Al-Buthom <ahmad.albuthom@inspirejo.com>
2023-09-06 12:35:45 +0300

    test

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-
 app.get("/chat-gpt", function (req, res) {
-    res.send("calling gpt service");
-  });
+  res.send("calling gpt service");
+});
+
+app.get("/echo", function (req, res) {
+  res.send(JSON.stringify(req, null, " "));
+});
 
 app.listen(8080);
