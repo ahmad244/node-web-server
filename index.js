60f1d14c37f83fd8770d7b044ddb4167496b9ea6
Ahmad Al-Buthom <ahmad.albuthom@inspirejo.com>
2023-09-06 16:09:17 +0300

    fixed /echo and print out body

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
@@ -10,8 +13,24 @@ app.get("/chat-gpt", function (req, res) {
   res.send("calling gpt service");
 });
 
-app.get("/echo", function (req, res) {
-  res.send(JSON.stringify(req, null, " "));
+app.post("/echo", function (req, res) {
+  // Create an object with selected properties from req
+  const reqData = {
+    method: req.method,
+    body: req.body,
+    t: "",
+    url: req.url,
+    headers: req.headers,
+    params: req.params,
+  };
+
+  // Stringify the modified object
+  res
+    .status(200)
+    .header({
+      "content-type": "application/json",
+    })
+    .send(JSON.stringify(reqData, null, " "));
 });
 
-app.listen(8080);
+app.listen(1684);
